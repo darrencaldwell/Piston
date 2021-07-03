93a47e8e144f410e7b276e94c9a084894523175f
CynicalCode21 <darrencaldwell00@outlook.com>
2021-07-03 15:12:34 +0100

    feat: added a POST new card with JSON validation

--- cards/server.ts
+++ cards/server.ts
@@ -1,12 +1,31 @@
 //const MongoClient = require("mongodb").MongoClient;
-const mongoose = require("mongoose")
-const express = require("express")
+const mongoose = require("mongoose");
+const express = require("express");
+const bodyParser = require("body-parser");
+const Validator = require("jsonschema").Validator;
 const app = express();
 
+const jsonParser = bodyParser.json();
+var validator = new Validator(); 
+
 const url = "mongodb://mongodb:27017/piston_cards";
-const port = 21450
+const port = 21450;
 const Schema = mongoose.Schema;
 
+var cardValidator = {
+  "id": "/Card",
+  "type": "object",
+  "properties": {
+    "name": {"type": "string"},
+    "description": {"type": "string"},
+    "repairCost": {"type": "string"},
+    "scrapValue": {"type": "string"},
+    "creditValue": {"type": "number"},
+    "cardType": {"type": "string"},
+    "lastEditor": {"type": "string"}
+  },
+  "required": ["name", "description", "repairCost", "scrapValue", "creditValue", "cardType", "lastEditor"]
+}
 
 const cardSchema = new mongoose.Schema({
   card: [{
@@ -48,6 +67,7 @@ const testCard = new Card({
   ]
 })
 
+
 app.get("/", function (req, res) {
   const date: Date = new Date();
   res.send(`ping pong the date is ${date}`)
@@ -64,11 +84,46 @@ app.get("/add", function (req, res) {
 })
 
 
-app.get("/card", (req, res) => {
+app.get("/cards/all", (req, res) => {
   Card.find((err, cards) => {
     if (err) res.send(err)
     else res.send(cards)
   })
+})
+
+
+app.post("/cards/new", jsonParser, (req, res) => {
+  const validation = validator.validate(req.body, cardValidator)
+  // if any errors in the json from the schema defined 
+  if (validation.errors.length) {
+    var errString: string = ""
+    validation.errors.forEach(arr => {errString += arr.message + "\n"})
+    res.send(errString);
+  }
+  // add to database
+  else {
+    const newCard = new Card ({
+        card: [
+          {
+          version: "1.0",
+          data: {
+            name: req.body.name,
+            description: req.body.description,
+            repairCost: req.body.repairCost,
+            scrapValue: req.body.scrapValue,
+            creditValue: req.body.creditValue,
+            type: req.body.cardType,
+            lastEditor: req.body.lastEditor,
+            dateModified: new Date()
+          }
+        }
+      ]
+    })
+    newCard.save((err, newCard) => {
+      if (err) res.send(err)
+      else res.send(`added: \n${newCard}`)
+    })
+  }
 })
 
 
